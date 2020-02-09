8ea65466a29a1fbc018c63fa95950007c96eb7c3
KIM HONGIL <clroyan01@gmail.com>
2020-02-09 22:48:37 +0900

    Create querydsl project

---
+++ src/main/java/boot/jpa/querydsl/QuerydslApplication.java
@@ -0,0 +1,13 @@
+package boot.jpa.querydsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuerydslApplication.class, args);
+    }
+
+}
