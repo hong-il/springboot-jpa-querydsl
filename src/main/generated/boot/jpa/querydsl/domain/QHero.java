a6fb39cf674d4871a5891afc86b7dcecf89453a5
KIM HONGIL <clroyan01@gmail.com>
2020-02-11 23:57:09 +0900

    Create querydsl domain - querydsl hero

---
+++ src/main/generated/boot/jpa/querydsl/domain/QHero.java
@@ -0,0 +1,43 @@
+package boot.jpa.querydsl.domain;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QHero is a Querydsl query type for Hero
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QHero extends EntityPathBase<Hero> {
+
+    private static final long serialVersionUID = -1603059142L;
+
+    public static final QHero hero = new QHero("hero");
+
+    public final NumberPath<Integer> age = createNumber("age", Integer.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public final StringPath note = createString("note");
+
+    public QHero(String variable) {
+        super(Hero.class, forVariable(variable));
+    }
+
+    public QHero(Path<? extends Hero> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QHero(PathMetadata metadata) {
+        super(Hero.class, metadata);
+    }
+
+}
+
